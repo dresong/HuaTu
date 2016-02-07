35465ff3209b536e9a424cccd669bc6322912580
xingkx <657808675@qq.com>
2016-02-07 11:23:46 +0800

    添加文件菜单栏，编辑菜单，文件图标

--- Draw/window.cpp
+++ Draw/window.cpp
@@ -7,19 +7,49 @@ Window::Window(QMainWindow *parent):
 {
     resize(400,300);
     m_menubar=this->menuBar();
+    connect(m_menubar, SIGNAL(triggered(QAction*)),this, SLOT(slotAction(QAction*)));
 
-    m_menu=new QMenu();
-    m_menu->setTitle("File");
-
+    //fileMenu
+    m_fileMenu=new QMenu();
+    m_fileMenu->setTitle("File");
     m_actNew=new QAction(0);
     m_actNew->setText("New");
-    QIcon icon(":/Icon/FileNew.png");
+    QIcon Inew(":/Icon/FileNew.png");
     m_actNew->setToolTip("chuang jian wenjian");
-    m_actNew->setIcon(icon);
-    m_menu->addAction(m_actNew);
-    m_menubar->addMenu(m_menu);
+    m_actNew->setIcon(Inew);
+    m_fileMenu->addAction(m_actNew);
+    m_menubar->addMenu(m_fileMenu);
 
+    m_actOpen = new QAction(0);
+    m_actOpen->setText("Open");
+    QIcon Iopen(":/Icon/FileOpen.png");
+    m_actOpen->setIcon(Inew);
+    m_fileMenu->addAction(m_actOpen);
+
+    m_actSave=new QAction(0);
+    m_actSave->setText("Save");
+    QIcon Isave(":/Icon/FileSave.png");
+    m_actSave->setIcon(Isave);
+
+    m_fileMenu->addAction(m_actSave);
+
+    m_actExit =new QAction(0);
+    m_actExit->setText("Exit");
+    m_fileMenu->addAction(m_actExit);
+
+    //editMenu
+    m_editMenu=new QMenu();
+    m_editMenu->setTitle("Edit");
+    m_menubar->addMenu(m_editMenu);
+    //boolBar
     tool=new QToolBar();
     tool->addAction(m_actNew);
+    tool->addAction(m_actSave);
+    connect(tool, SIGNAL(actionTriggered(QAction*)), this, SLOT(slotAction(QAction*)));
     this->addToolBar(tool);
+}
+#include <QDebug>
+void Window::slotAction(QAction *action)
+{
+    qDebug()<<Q_FUNC_INFO<< action->text();
 }
--- Draw/window.h
+++ Draw/window.h
@@ -4,12 +4,22 @@
 
 class Window : public QMainWindow
 {
+    Q_OBJECT
 public:
     explicit Window(QMainWindow *parent=0);
+
+public slots:
+    void slotAction(QAction* action);
+
 private:
     QMenuBar *m_menubar;
-    QMenu *m_menu;
+    QMenu *m_fileMenu;
+    QMenu *m_editMenu;
     QAction *m_actNew;
+    QAction *m_actSave;
+    QAction *m_actOpen;
+    QAction *m_actExit;
+
     QToolBar *tool;
 
 };
